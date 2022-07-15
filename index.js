a2b6610ca48a643b232b41f38fad0dbbe11a773c
okafor-zag <85526376+okafor-zag@users.noreply.github.com>
2022-07-15 16:31:50 +0100

    changes

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-var smit = document.getElementById("submit");
-smit.addEventListener("click",formvalidate,false); 
+const username = document.querySelector('input');
+const password = document.getElementById('password');
 
 
-function formvalidate(){
-
-    console.log(" vua moi click submit");
-
-    if(t==null){
-        var t= document.forms["myForm"]["fname"].value;
-        if(t==null || t==""){
-             alert("xin dien vao email");
-
-        }
+const validateAccount = (input) => {
+    if(input.length < 0 || input == null || input == "") {
+        alert('Please enter a username and password');
     }
+};
 
-}
+validateAccount(username);
